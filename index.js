16ec00a4db9db964730b5cecce832c1b3cb971f3
unknown <MHasan@MHASAN.pmci.local>
2017-01-09 13:32:35 -0600

    working correctly

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 function setGrid(width, height) {
 
 for (var i=0; i < height*width; i++) {
-  $('.container').append('<div class = "tile"></div>');
+  $('.container').append('<div class = "tile" data-label="' + i + '"></div>');
 }
 
 $('.tile').css("width", 100/width + "%");
-$('.tile').css("height", 100/width + "%");
+$('.tile').css("height", 100/height + "%");
+
+$('.container').css("height", height*40 + "px");
+$('.container').css("width", width*40 + "px");
 
 for (var i=0; i < height*width; i++) {
-  $('.upper').append('<button class = "upper-tile"></button>');
+  $('#upper').append('<button class = "upper-tile" data-label="' + i + '"></button>');
 }
 
+$('#upper').css("height", height*40 + "px");
+$('#upper').css("width", width*40 + "px");
+
 $('.upper-tile').css("width", 100/width + "%");
-$('.upper-tile').css("height", 100/width + "%");
+$('.upper-tile').css("height", 100/height + "%");
+
 }
 
 function setMines(count) {
@@ -111,16 +118,85 @@ for(var i=0; i < tileCount; i++) {
     }
   }
 
-function iterateSquare() {
+function groupClear(data, height, width) { //floodfill
+  if (data < 0 || data > height * width) {
+    return;
+  }
+  if ($('.tile[data-label="' + data + '"]').html() != "") { //numbered or bomb square
+    if (!$('[data-label="' + data + '"]').hasClass('bomb')) { //clear if number and stop
+      $('.upper-tile[data-label="' + data + '"]').css('visibility', 'hidden');
+    }
 
+    return;
+  }
+
+  if ($('.upper-tile[data-label="' + data + '"]').css('visibility') == 'hidden') { //tile has already been uncovered.
+    return;
+  }
+  $('.upper-tile[data-label="' + data + '"]').css('visibility', 'hidden');
+  if ((data + 1) % width !== 0) {
+     groupClear(data+1, height, width); //east
+  }
+  if ((data) % width !== 0) {
+    groupClear(data-1, height, width); //west
+  }
+    groupClear((parseInt(data)+parseInt(height)), height, width); //south; wont work without casting.
+    groupClear(data-height, height, width); //north
 }
 
 
 
+function evaluate() { //Get count of remaining buttons and check if the game has been won.
+  var tileCount = $('.tile').length;
+  var count = 0;
+  var bombCount = 0;
+  for (var i = 0; i < tileCount; i++) {
+    if ($('.upper-tile[data-label="' + i + '"]').css('visibility') != 'hidden') {
+      count++;
+      if ($('[data-label="' + i + '"]').hasClass('bomb')) {
+        bombCount++;
+      }
+    }
+  }
+  return bombCount == count-1;
+}
+
+function game() {
+  var height = prompt("Enter the game height");
+  var width = prompt("Enter the game width");
+  //var height = 2;
+  //var width = 2;
+  setGrid(height,width);
+  var mines = Math.ceil((height*width) * (5/32));
+  setMines(mines);
+  setNumbers(height,width);
+
+$(document).on('click', '.upper-tile', function() {
+  
+  var data = $(this).data("label");
+  if ($('[data-label="' + data + '"]').hasClass('bomb')) { //bomb click
+    $('[data-label="' + data + '"]').css('background-color', 'red');
+    var tileCount = $('.tile').length;
+    
+    for (var i = 0; i < tileCount; i++) { //show all the bombs
+      if ($('[data-label="' + i + '"]').hasClass('bomb')) {
+        $('.upper-tile[data-label="' + i + '"]').css('visibility', 'hidden');
+      } else {
+        $('.upper-tile[data-label="' + i + '"]').prop("disabled",true); //don't allow any further clicks.
+      }
+    }
+  }
+
+    if (evaluate()) { //Game has been won.
+      for (var i =0; i <  $('.tile').length; i++) {
+        $('.upper-tile[data-label="' + i + '"]').prop("disabled",true); //don't allow any further clicks.
+          $('.upper-tile[data-label="' + i + '"]').html('&#9873;');
+      }
+  }
+  groupClear(data, height, width); 
+});
+}
+
 $(document).ready(function() {
-  setGrid(9,9);
-  setMines(10);
-  setNumbers(9,9);
-
-
+  game();
 });
